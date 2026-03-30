131f0c7ae8d59f57be598a5d0ac7168fb2bc7e12
Dylan <dinghuangzhang1993@gmail.com>
2026-03-30 21:55:20 +0100

    refactor: migrate privacy policy from internal component to static HTML page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { I18nProvider } from "./i18n";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -7,36 +6,8 @@ import Portfolio from "./components/Portfolio";
 import TechStack from "./components/TechStack";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
-import PrivacyPolicy from "./components/PrivacyPolicy";
-
-// 简单的 hash 路由
-function useHashRoute() {
-    const [route, setRoute] = useState(window.location.hash || "#/");
-
-    useEffect(() => {
-        const handleHashChange = () => {
-            setRoute(window.location.hash || "#/");
-            window.scrollTo(0, 0);
-        };
-        window.addEventListener("hashchange", handleHashChange);
-        return () => window.removeEventListener("hashchange", handleHashChange);
-    }, []);
-
-    return route;
-}
 
 export default function App() {
-    const route = useHashRoute();
-
-    // 隐私政策页面 - 独立布局
-    if (route === "#/privacy" || route === "#/privacy-policy") {
-        return (
-            <I18nProvider>
-                <PrivacyPolicy />
-            </I18nProvider>
-        );
-    }
-
     // 主页
     return (
         <I18nProvider>
